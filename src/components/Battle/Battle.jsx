7ba0e8592f0893e49b281d86c3ac6953f8fc5ecc
Zach Carter <thezachcarter@gmail.com>
2022-04-17 08:58:51 -0500

    add logic to loop through gods and calculate damage to monster

--- src/components/Battle/Battle.jsx
+++ src/components/Battle/Battle.jsx
@@ -18,21 +18,86 @@ function Battle(props) {
   console.log('in BATTLE', monsterArray, godArray);
 
   const attack = () => {
+    console.log('attack clicked');
 
+    //power level of monster
     const monsterPower = monsterArray[0].power
-
+    //total power level of all gods
     const godPower = godArray.reduce((accumulator, object) => {
       return accumulator + object.power;
     }, 0);
-
-    console.log('attack clicked');
+    
+    //determine victory status by checking power of monster and ALL gods 
     if(monsterPower === 0){
       console.log('Monster:', monsterPower,'Gods:', godPower, 'Victory');
     } else if (godPower === 0)
       {console.log('Monster:', monsterPower,'Gods:', godPower,'Defeat')
     } else {
       console.log('Monster:', monsterPower,'Gods:', godPower, 'The battle continues!');
-    }
+    };
+    
+    //base damageToMonster, before checking element or culture = 2
+    let damageToMonster = 2;
+    
+    //for loop to set damageToMonsterToMonster based on element comparison
+    for(let i = 0; i < godArray.length; i++){
+      console.log(godArray[i]);
+      //check if individual god is defeated
+      if(godArray[i].power === 0){
+        console.log(godArray[i].name,'DEFEATED');
+      } else {
+        switch (godArray[i].element) {
+          case 'Earth':
+            if(monsterArray[0].element === 'Earth'){
+              damageToMonster = 2; 
+            } else if(monsterArray[0].element === 'Sky'){
+              damageToMonster = 3; 
+            }else if(monsterArray[0].element === 'Fire'){
+              damageToMonster = 1; 
+            }else if(monsterArray[0].element === 'Water'){
+              damageToMonster = 2; 
+            } else {damageToMonster = 2};
+          case 'Sky':
+            if(monsterArray[0].element === 'Sky'){
+              damageToMonster = 2; 
+            } else if(monsterArray[0].element === 'Water'){
+              damageToMonster = 3; 
+            }else if(monsterArray[0].element === 'Earth'){
+              damageToMonster = 1; 
+            }else if(monsterArray[0].element === 'Fire'){
+              damageToMonster = 2; 
+            } else {damageToMonster = 2};
+          case 'Fire':
+            if(monsterArray[0].element === 'Fire'){
+              damageToMonster = 2; 
+            } else if(monsterArray[0].element === 'Earth'){
+              damageToMonster = 3; 
+            }else if(monsterArray[0].element === 'Water'){
+              damageToMonster = 1; 
+            }else if(monsterArray[0].element === 'Sky'){
+              damageToMonster = 2; 
+            } else {damageToMonster = 2};
+          case 'Water':
+            if(monsterArray[0].element === 'Water'){
+              damageToMonster = 2; 
+            } else if(monsterArray[0].element === 'Fire'){
+              damageToMonster = 3; 
+            }else if(monsterArray[0].element === 'Sky'){
+              damageToMonster = 1; 
+            }else if(monsterArray[0].element === 'Earth'){
+              damageToMonster = 2; 
+            } else {damageToMonster = 2};   
+          default:
+            damageToMonster = 2;
+        }//end switch statement checking god element
+
+      //damage multiplier for culture match
+      if(godArray[i].culture === monsterArray[0].culture){
+        damageToMonster *= 2;
+        }
+        console.log(godArray[i].name, damageToMonster);
+      }//end if else checking god disabled/enabled
+    }//end for loop of godArray
   }
   
 
