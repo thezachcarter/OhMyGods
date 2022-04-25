cf64e74985f413a930aae839566ef37a5ee68a54
Zach Carter <thezachcarter@gmail.com>
2022-04-25 11:26:29 -0500

    generat random numbers from click on x button that match withunsued go ids

--- src/components/GodCard/GodCard.jsx
+++ src/components/GodCard/GodCard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {useHistory, useLocation} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory, useLocation } from 'react-router-dom';
 import { attack } from '../Battle/Battle';
 import { renderUserDisplay } from '../UserPage/UserPage';
 import axios from 'axios';
@@ -11,7 +11,7 @@ import './GodCard.scss'
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
 // component name TemplateFunction with the name for the new component.
-function GodCard({attack, renderUserDisplay}) {
+function GodCard({ attack, renderUserDisplay }) {
   // Using hooks we're creating local state for a "heading" variable with
   // a default value of 'Functional Component'
   const store = useSelector((store) => store);
@@ -24,30 +24,34 @@ function GodCard({attack, renderUserDisplay}) {
   const godInfo = store.godInfo;
 
   useEffect(() => {
-    
+
     checkInBattle();
     dispatch({ type: 'GET_USERS_GODS', payload: user.id });
     console.log('**********************GOD CARD USE EFFECT! user:', user, 'godArray:', godArray.length);
-    
+
   }, []);
 
-  const populateNewUserGods = () => {if(godArray?.length < 4){
-    dispatch({type: 'POPULATE_GODS'});}
+  const populateNewUserGods = () => {
+    if (godArray?.length < 4) {
+      dispatch({ type: 'POPULATE_GODS' });
+    }
   }
-  
+
   const checkInBattle = () => {
-    if(location.pathname === '/battle'){
+    if (location.pathname === '/battle') {
       setInBattle(true);
       console.log('inBattle?', inBattle);
     }
   };
 
   const increasePower = (god, updatedGodPower) => {
-    if (user.devotion > 0){updatedGodPower += 1;
-    console.log('increasePower', god, updatedGodPower);
-    dispatch({ type: 'UPDATE_USER_GOD_POWER', payload: god, updatedGodPower})
-    decreaseDevotion(user.id, user.devotion)}
-    else{
+    if (user.devotion > 0) {
+      updatedGodPower += 1;
+      console.log('increasePower', god, updatedGodPower);
+      dispatch({ type: 'UPDATE_USER_GOD_POWER', payload: god, updatedGodPower })
+      decreaseDevotion(user.id, user.devotion)
+    }
+    else {
       alert('you are out of devotion points')
     }
   };
@@ -55,78 +59,105 @@ function GodCard({attack, renderUserDisplay}) {
   const decreaseDevotion = (userId, updatedDevotion) => {
     updatedDevotion -= 1;
     // console.log('decreaseDevotion', userId, updatedDevotion);
-    dispatch({ type: 'UPDATE_DEVOTION', payload: userId, updatedDevotion})
+    dispatch({ type: 'UPDATE_DEVOTION', payload: userId, updatedDevotion })
   };
 
   const handleGodInfo = (godName) => {
     console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!handleGodInfo clicked');
     axios.get(`/api/info/${godName}`)
-    .then( response => { 
-        dispatch({type: 'SET_GOD_INFO_STORE', payload: response.data.query.pages})
+      .then(response => {
+        dispatch({ type: 'SET_GOD_INFO_STORE', payload: response.data.query.pages })
         console.log(response.data.query.pages);
-    })
-    .then( response => { 
-      renderUserDisplay('displayGodInfo');
-    })
-    .catch(err => {
+      })
+      .then(response => {
+        renderUserDisplay('displayGodInfo');
+      })
+      .catch(err => {
         console.log(err)
-    })
-    
+      })
   };
-  
+
+  const handleReplaceGod = () => {
+
+    const godIds = [godArray[0].god_id, godArray[1].god_id, godArray[2].god_id, godArray[3].god_id,];
+    const len = 4;
+    const newGodIds = [];
+    for (let i = 0; i < len;){
+      const random = Math.floor(Math.random() * (16 - 1)) + 1;
+      if (!godIds.includes(random) &&
+          !newGodIds.includes(random)){
+            newGodIds.push(random);
+            i++;
+      }
+    }
+    console.log(newGodIds);
+
+    // let newGodsIds = [];
+
+    // for (let godId of godIds) {
+    //   let randNum = Math.floor(Math.random() * (16 - 1)) + 1;
+    //   for (let i = 0; newGodsIds < 3; i++) {
+    //     if (randNum !== godId && randNum !== newGodId) {
+    //       newGodsIds.push(randNum)
+    //     }
+    //   }
+    // }
+    // console.log(newGodsIds);
+  }
+
   return (
     <>
-    {/* ternary to check for battle and render cards accordingly */}
-    {inBattle ?
-      
-      <div className="godCardContainer ">
-      {godArray.map(god => {
-        return (
-          
-          <div className={`godCard ${god.element}`} onClick={((event) => attack(god.id))} key={god.id}>
-            <h2 className="godName">{god.name}</h2>
-            <img
-              className="godImg"
-              src={god.image}
-              alt={god.name}
-            /> 
-            <h2 className="godCulture">{god.culture}</h2>
-            <h2 className="godPower">{god.power}</h2>
-            <p></p>
-          </div>
-        );
-      })}
-    </div>
+      {/* ternary to check for battle and render cards accordingly */}
+      {inBattle ?
 
-    :
+        <div className="godCardContainer ">
+          {godArray.map(god => {
+            return (
 
-    <div className="godCardContainer">
-      {godArray.map(god => {
-        return (
+              <div className={`godCard ${god.element}`} onClick={((event) => attack(god.id))} key={god.id}>
+                <h2 className="godName">{god.name}</h2>
+                <img
+                  className="godImg"
+                  src={god.image}
+                  alt={god.name}
+                />
+                <h2 className="godCulture">{god.culture}</h2>
+                <h2 className="godPower">{god.power}</h2>
+                <p></p>
+              </div>
+            );
+          })}
+        </div>
 
-          <div className={`godCard ${god.element}`}key={god.id}>
-            <h2 className="godName">{god.name}</h2>
-            <img
-              className="godImg"
-              src={god.image}
-              alt={god.name}
-            /> 
-            <h2 className="godCulture">{god.culture}</h2>
-            <h2 className="godPower">{god.power}</h2>
-            <button className="godCardBtn">X</button>
-            <button className="godCardBtn" onClick={(() => increasePower(god, god.power))}>^</button>
-            <button className="godCardBtn" onClick={(() => handleGodInfo(god.name))}>?</button>
-          </div>
-        );
-      })}
-    </div>
+        :
 
-    }
-    {/* end inBattle ternary  */}
+        <div className="godCardContainer">
+          {godArray.map(god => {
+            return (
 
-  </>
+              <div className={`godCard ${god.element}`} key={god.id}>
+                <h2 className="godName">{god.name}</h2>
+                <img
+                  className="godImg"
+                  src={god.image}
+                  alt={god.name}
+                />
+                <h2 className="godCulture">{god.culture}</h2>
+                <h2 className="godPower">{god.power}</h2>
+                <button className="godCardBtn" onClick={(() => handleReplaceGod())}>X</button>
+                <button className="godCardBtn" onClick={(() => increasePower(god, god.power))}>^</button>
+                <button className="godCardBtn" onClick={(() => handleGodInfo(god.name))}>?</button>
+              </div>
+            );
+          })}
+        </div>
+
+      }
+      {/* end inBattle ternary  */}
+
+    </>
   );//end return
-  
+
 }
 
 export default GodCard;
