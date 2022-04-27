42046a01119d72f0ff8a3df61433bb704b108e14
Zach Carter <thezachcarter@gmail.com>
2022-04-27 11:33:05 -0500

    add table of monsters to display

--- src/components/MonsterDisplay/MonsterDisplay.jsx
+++ src/components/MonsterDisplay/MonsterDisplay.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React, {useEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 
 function MonsterDisplay(props) {
-  const dispatch = useDispatch();
-  return (
-    <h2>MONSTER DISPLAY</h2>
-  );
+
+    useEffect(() => {
+        dispatch({ type: 'GET_USERS_MONSTERS'});
+      }, []);
+
+    const dispatch = useDispatch();
+    const store = useSelector((store) => store);
+
+    const monsters = store.usersMonsters;
+
+    console.log('--------------------------',monsters);
+    return (
+
+        <div className="monsterDisplay">
+            <h2>MONSTER DISPLAY</h2>
+            <h2>Conquered Monster</h2>
+                <table className="monsterTbl">
+                    <tbody>
+                        {monsters.map(monster => {
+                            return (
+                                <tr key={monster.id} className={monster.element}>
+                                    <td>{monster.name}</td>
+                                    <td>{monster.culture}</td>
+                                    <td>{monster.element}</td>
+                                </tr>
+                            )
+                        })}
+                        
+                    </tbody>
+                </table>
+            </div>
+    );
 }
 
 export default MonsterDisplay;
